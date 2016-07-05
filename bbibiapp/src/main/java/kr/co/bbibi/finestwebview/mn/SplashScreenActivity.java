47c5c1e88cd38db65449586a10681c86af975d38
tobby2002 <tobby2002@gmail.com>
2016-07-06 03:26:28 +0900

    finestwebview-android-master's Bbibi version
    For Bbibi app
    We customized finestwebview-android-master

---
+++ bbibiapp/src/main/java/kr/co/bbibi/finestwebview/mn/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package kr.co.bbibi.finestwebview.mn;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+/**
+ * Created by iosdeveloper on 2016. 6. 17..
+ */
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
